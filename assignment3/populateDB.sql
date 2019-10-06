3c921d261fd0183ae59684f0a982aed847a90c90
ameyabp <ameyapatil249@gmail.com>
2019-10-05 22:09:51 -0400

    More scripts to wrangle and load db

---
+++ assignment3/populateDB.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Storms(
+	eventID	int PRIMARY KEY,
+	eventType	varchar(30),
+	state	varchar(30),
+	stateFips	int,
+	czmType	char(1),
+	countyFips	int,
+--	countyName	varchar(100),
+	lat	float,
+	lng	float,
+	beginDate	char(10),
+	endDate	char(10),
+	injuries	int,
+	deaths	float,
+	damage	float
+);
+
+\copy Storms (eventID, eventType, state, stateFips, czmType, countyFips, lat, lng, beginDate, endDate, injuries, deaths, damage) FROM 'compositeStorms.csv' WITH delimiter ',' csv header;
